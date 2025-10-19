d6cd92c2da84eb11c04ab2bf7b3537a70ee01209
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-20 01:11:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "neondb_owner", // username
-  host: "ep-bitter-dew-ad8ohllq-pooler.c-2.us-east-1.aws.neon.tech", // updated host
+  host: "ep-twilight-frog-addghbvs-pooler.c-2.us-east-1.aws.neon.tech", // updated host
   database: "neondb", // database name
-  password: "npg_TLtl3vKZ7Sue", // updated password
+  password: "npg_9HdagToMF5Jx", // updated password
   port: 5432, // default postgres port
   ssl: { rejectUnauthorized: false }, // ssl required
 });
