82331ad2d6c0032fda8891bc50c6b2370bfeb186
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-20 09:59:40 +0530

    Update PieChart.js

--- src/DashboardADmin/PieChart.js
+++ src/DashboardADmin/PieChart.js
@@ -11,7 +11,7 @@ useEffect(() => {
 const fetchData = async () => {
 
 try {
-const response = await fetch("https://antara-gug4.onrender.com/fetchCutomerOrder");
+const response = await fetch("https://omega-zg6z.onrender.com/fetchCutomerOrder");
 const data = await response.json();
 
 if (!data.products || !Array.isArray(data.products)) {
