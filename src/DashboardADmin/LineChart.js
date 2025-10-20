b4f5b083cb4bd190b11e6bac2ea64a10fc441bb9
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-20 09:59:16 +0530

    Update LineChart.js

--- src/DashboardADmin/LineChart.js
+++ src/DashboardADmin/LineChart.js
@@ -36,7 +36,7 @@ data: [],
 useEffect(() => {
 const customerFetch = async () => {
 try {
-const response = await fetch("https://antara-gug4.onrender.com/fetchCutomerOrder");
+const response = await fetch("https://omega-zg6z.onrender.com/fetchCutomerOrder");
 const data = await response.json();
 
 // Month-wise 
