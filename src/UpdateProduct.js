5db4b117bd31a768b0ae8111209b10fb46b13e4c
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-10-20 10:11:34 +0530

    Update UpdateProduct.js

--- src/UpdateProduct.js
+++ src/UpdateProduct.js
@@ -27,7 +27,7 @@ formData.append("image", productImage);
 }
 
 try {
-const response = await fetch("https://antara-gug4.onrender.com/api/update-product", {
+const response = await fetch("https://omega-zg6z.onrender.com/api/update-product", {
 method: "POST",
 body: formData,
 });
